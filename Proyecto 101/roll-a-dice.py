0c2cb95e8d456a81124c088adde06fc6f63a8676
NewPackAttack <107285687+NewPackAttack@users.noreply.github.com>
2022-11-11 16:01:02 -0700

    Add files via upload

---
+++ Proyecto 101/roll-a-dice.py
@@ -0,0 +1,43 @@
+#esto me recuerda a cuphead xd
+import random
+
+response = input("y")
+
+no = random.randint(1,6)
+
+if no == 1:
+    print("[-----]")
+    print("[     ]")
+    print("[  0  ]")
+    print("[     ]")
+    print("[-----]")
+if no == 2:
+    print("[-----]")
+    print("[     ]")
+    print("[  1  ]")
+    print("[     ]")
+    print("[-----]")
+if no == 3:
+    print("[-----]")
+    print("[     ]")
+    print("[  2  ]")
+    print("[     ]")
+    print("[-----]")
+if no == 4:
+    print("[-----]")
+    print("[     ]")
+    print("[  3  ]")
+    print("[     ]")
+    print("[-----]")
+if no == 5:
+    print("[-----]")
+    print("[     ]")
+    print("[  4  ]")
+    print("[     ]")
+    print("[-----]")
+if no == 6:
+    print("[-----]")
+    print("[     ]")
+    print("[  5  ]")
+    print("[     ]")
+    print("[-----]")
